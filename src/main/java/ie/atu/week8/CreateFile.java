f4760f1729fbc7453bdb5867c6dc563c2d0a2111
G00405014 <G00405014@atu.ie>
2022-11-21 10:01:47 +0000

    Append file

--- src/main/java/ie/atu/week8/CreateFile.java
+++ src/main/java/ie/atu/week8/CreateFile.java
@@ -1,12 +1,22 @@
 package ie.atu.week8;
 
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class CreateFile {
     public static void main(String[] args) {
-        {
+
             File myFile = new File("MyFile.txt");
             System.out.println("My file is located at " + myFile.getAbsolutePath());
-        }
+            try {
+                FileWriter myWriter = new FileWriter(myFile, true);
+                myWriter.write("g");
+                myWriter.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
     }
 }
+
