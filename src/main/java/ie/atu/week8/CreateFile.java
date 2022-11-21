7adb4c883d87ddc2ced84ceb096e3c624483dc49
G00405014 <G00405014@atu.ie>
2022-11-21 10:59:34 +0000

    Application

--- src/main/java/ie/atu/week8/CreateFile.java
+++ src/main/java/ie/atu/week8/CreateFile.java
@@ -1,23 +1,39 @@
 package ie.atu.week8;
 
 import java.io.*;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.util.Scanner;
 
-public class CreateFile {
+public class creatFile {
     public static void main(String[] args) {
 
-            File myFile = new File(myFile.txt);
-            System.out.println("My file is located " + myFile.getAbsolutePath());
-            try {
-                FileWriter myWriter = new FileWriter(myFile, true);
-                PrintWriter myPrinter = new PrintWriter("Names.txt");
-                myPrinter.println("This is my fourth line");
-                myPrinter.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+        System.out.println("Enter file name");
+        Scanner input = new Scanner(System.in);
+        String name = input.nextLine();
+        File myFile = new File(name);
+        System.out.println("My file is located at" + myFile.getAbsolutePath());
+
+        try {
+            PrintWriter myWriter = new PrintWriter(new FileWriter("myFile.txt", true));
+            myWriter.println("this is my first line");
+            myWriter.close();
+
+
+        }catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        Scanner inputFile = null;
+        try{
+            inputFile = new Scanner(new File("myFile.txt"));
+        }catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        while (inputFile.hasNext())
+        {
+            String str = inputFile.nextLine();
+            System.out.println(str);
+        }
 
     }
 }
