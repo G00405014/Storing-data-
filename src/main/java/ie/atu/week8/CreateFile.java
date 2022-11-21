4c967869fb086938cb51ade307f1ff4a4f377923
G00405014 <G00405014@atu.ie>
2022-11-21 10:22:50 +0000

    Print Writer Class

--- src/main/java/ie/atu/week8/CreateFile.java
+++ src/main/java/ie/atu/week8/CreateFile.java
@@ -1,5 +1,6 @@
 package ie.atu.week8;
 
+import java.io.*;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -7,12 +8,13 @@ import java.io.IOException;
 public class CreateFile {
     public static void main(String[] args) {
 
-            File myFile = new File("MyFile.txt");
-            System.out.println("My file is located at " + myFile.getAbsolutePath());
+            File myFile = new File(myFile.txt);
+            System.out.println("My file is located " + myFile.getAbsolutePath());
             try {
                 FileWriter myWriter = new FileWriter(myFile, true);
-                myWriter.write("g");
-                myWriter.close();
+                PrintWriter myPrinter = new PrintWriter("Names.txt");
+                myPrinter.println("This is my fourth line");
+                myPrinter.close();
             } catch (IOException e) {
                 e.printStackTrace();
             }
